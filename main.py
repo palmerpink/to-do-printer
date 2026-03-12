9aaf4bb0de8e7b7767c49f35dcdd244a597b5113
palmerpink <chrispalmer0000@gmail.com>
2026-03-12 16:06:55 -0400

    print 1 task per page

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from os import environ
 from dotenv import load_dotenv
 from todoist_api_python.api import TodoistAPI
+import wx
+from wx.html import HtmlEasyPrinting
 
 load_dotenv()
+
+app = wx.App()
 
 api = TodoistAPI(environ.get("TODO_API"))
 
 tasks = api.filter_tasks(query="today")
-    
+
 for list in tasks:
     for task in list:
-        print(f"TSK: {task.content}\nTID: {task.id}\nDES: {task.description}\nPRI: {task.priority}\nLAB: {task.labels}\nPAR: {task.parent_id}\n\n")
-
-#comments_iter = api.get_comments(task_id=task.id)
-#for comments in comments_iter:
-#    for comment in comments:
-#        print(f"Comment: {comment.content}")
-#project_id="6fqqv4CQvFxXqJCJ")
+        todos = f"""
+        <h1>{task.content}</h1>
+        <p>
+            TID: {task.id}<br>
+            DES: {task.description}<br>
+            PRI: {task.priority}<br>
+            LAB: {task.labels}<br>
+            PAR: {task.parent_id}
+        </p>
+        """
+        printer = HtmlEasyPrinting()
+        printer.GetPageSetupData().SetMarginBottomRight(wx.Point(2, 2))
+        printer.GetPageSetupData().SetMarginTopLeft(wx.Point(2, 2))
+        printer.GetPageSetupData().SetPaperSize(wx.Size(80, 80))
+        printer.SetPromptMode(HtmlEasyPrinting.Prompt_Never)
+        printer.PrintText(todos)
+        app.MainLoop()
