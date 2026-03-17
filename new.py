fa3725b47a1cf062e3a8c1a3da31b3c7e1289f27
palmerpink <chrispalmer0000@gmail.com>
2026-03-17 14:08:39 -0400

    added "new" scripts to automate printing new tasks over the course of the day, then removing the "new" label

---
+++ new.py
@@ -0,0 +1,35 @@
+from os import environ
+from dotenv import load_dotenv
+from todoist_api_python.api import TodoistAPI
+import wx
+from wx.html import HtmlEasyPrinting
+
+load_dotenv()
+
+app = wx.App()
+
+api = TodoistAPI(environ.get("TODO_API"))
+
+tasks = api.filter_tasks(query="@new")
+
+for list in tasks:
+    for task in list:
+        todos = f"""
+        <h1>{task.content}</h1>
+        <p>
+            <b>TID:</b> {task.id}<br>
+            <b>DES:</b> {task.description}<br>
+            <b>PRI:</b> {task.priority}<br>
+            <b>LAB:</b> {", ".join(task.labels)}<br>
+            <b>PAR:</b> {task.parent_id}
+        </p>
+        """
+        printer = HtmlEasyPrinting()
+        printer.GetPageSetupData().SetMarginBottomRight(wx.Point(2, 2))
+        printer.GetPageSetupData().SetMarginTopLeft(wx.Point(2, 2))
+        printer.GetPageSetupData().SetPaperSize(wx.Size(80, 80))
+        printer.SetPromptMode(HtmlEasyPrinting.Prompt_Never)
+        printer.PrintText(todos)
+        app.MainLoop()
+
+api.remove_shared_label("new")
---
+++ run_new.bat
@@ -0,0 +1,6 @@
+@echo off
+
+.venv\Scripts\python.exe new.py
+
+REM The script automatically exits the venv context when finished
+exit
